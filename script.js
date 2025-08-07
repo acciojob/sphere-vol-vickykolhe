6c8b87f761a2f0e8e838660595086e3d85c2bf85
Babusha Kolhe <165826953+vickykolhe@users.noreply.github.com>
2025-08-07 09:51:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 
 
 function volume_sphere() {
-	let raddd=document.getElementById("radius").value;
-let vol = document.getElementById("volume");
-	let r=Number(raddd);
-	 if (isNaN(r) || r < 0) {
-                vol.value = "NaN";
-                return;
-            }
-	 let volume = (4 / 3) * Math.PI * Math.pow(r, 3);
-	vol.value = volume.toFixed(4);
+    event.preventDefault(); 
+
+    let raddd = document.getElementById("radius").value;
+    let vol = document.getElementById("volume");
+
+    let r = Number(raddd);
+    if (isNaN(r) || r < 0) {
+        vol.value = "NaN";
+        return;
+    }
+
+    let volume = (4 / 3) * Math.PI * Math.pow(r, 3);
+    vol.value = volume.toFixed(4);
   
 } 
 
